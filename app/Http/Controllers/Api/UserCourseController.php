ffc384218ac77088d47cf663a1357479058eb628
husseinadkerning <hseneldowi@gmail.com>
2022-03-27 19:58:39 +0300

    create storeUserCourseRequest to validate course id in store user course endpoint

--- app/Http/Controllers/Api/UserCourseController.php
+++ app/Http/Controllers/Api/UserCourseController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserCourseRequest;
 use App\Http\Resources\Course\courseDetailResource;
 use App\Http\Resources\UserCourse\userCourseResource;
 use Illuminate\Http\Request;
 
 class UserCourseController extends Controller
 {
-    public function store(Request $request){
+    public function store(StoreUserCourseRequest $request){
 
         $data = auth()->user()->courses()->sync($request->course_id);
 
