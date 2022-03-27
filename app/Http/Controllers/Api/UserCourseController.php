75e6e6bdc37ec13e41b589792d722a8c57659c9e
husseinadkerning <hseneldowi@gmail.com>
2022-03-27 20:08:14 +0300

    add remove user course endpoint

--- app/Http/Controllers/Api/UserCourseController.php
+++ app/Http/Controllers/Api/UserCourseController.php
@@ -3,21 +3,35 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreUserCourseRequest;
+use App\Http\Requests\UserCourseRequest;
 use App\Http\Resources\Course\courseDetailResource;
 use App\Http\Resources\UserCourse\userCourseResource;
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class UserCourseController extends Controller
 {
-    public function store(StoreUserCourseRequest $request){
+    //store course to user course list
+    public function store(UserCourseRequest $request){
 
-        $data = auth()->user()->courses()->sync($request->course_id);
+        $data = auth()->user()->courses()->syncWithoutDetaching($request->course_id);
 
         return response()->json([
             'status_code' => 200,
             'message' => 'Course was added successfully to user course list.',
             'data' =>  ['course_id' => $request->course_id,]
+        ]);
+    }
+
+    //remove course to user course list
+    public function destroy($course_id){
+
+        $data = auth()->user()->courses()->detach($course_id);
+
+        return response()->json([
+            'status_code' => 200,
+            'message' => 'Course was removed successfully from user course list.',
+            'data' =>  ['course_id' => $course_id,]
         ]);
     }
 
