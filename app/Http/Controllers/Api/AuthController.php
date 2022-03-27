50dde42759ae91603b946c45e9e3be2bfc9d31af
husseinadkerning <hseneldowi@gmail.com>
2022-03-27 15:24:10 +0300

    update register method

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
@@ -24,11 +25,6 @@ class AuthController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-
-        $defaultCategories = Category::whereNull('user_id')->get();
-        foreach ($defaultCategories as $category) {
-            $user->categories()->create($category->toArray());
-        }
 
         return $user->createToken($request->device_name)->plainTextToken;
     }
