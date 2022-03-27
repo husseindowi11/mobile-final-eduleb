a34d7d4decdbd9fd44b970889c6bdf509f33dfb1
husseinadkerning <hseneldowi@gmail.com>
2022-03-27 18:50:18 +0300

    add course controller

---
+++ app/Http/Controllers/Api/CourseController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+
+}
