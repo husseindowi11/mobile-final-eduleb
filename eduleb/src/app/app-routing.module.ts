bd9aa57f21f93c8da42490654cc159a3ac6a6c12
husseinadkerning <hseneldowi@gmail.com>
2022-04-16 16:00:23 +0300

    add home page

--- eduleb/src/app/app-routing.module.ts
+++ eduleb/src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
-  }
+  },
 ];
 @NgModule({
   imports: [
