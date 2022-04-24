b97d8a630db45003f5a51f7bf1f42673debaa962
husseinadkerning <hseneldowi@gmail.com>
2022-04-24 04:16:17 +0300

    link course details to apis

--- eduleb/src/app/course-detail/course-detail.page.ts
+++ eduleb/src/app/course-detail/course-detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Course, CourseService } from '../apis/course.service';
 
 @Component({
   selector: 'app-course-detail',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseDetailPage implements OnInit {
 
-  constructor() { }
+  details;
+
+  constructor(private router: Router,private route: ActivatedRoute, private service:CourseService) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.queryParamMap.get('course');
+    this.service.getCourse(id).subscribe( response => {
+      this.details = response['data'];
+      console.log(this.details);
+    });
   }
 
 }
