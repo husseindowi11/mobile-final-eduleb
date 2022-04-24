20fabad5c29faa7bc1d694b30f114f8924401a80
husseinadkerning <hseneldowi@gmail.com>
2022-04-24 18:32:32 +0300

    redirect on login success

--- eduleb/src/app/login/login.page.ts
+++ eduleb/src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
 import { AuthenticationService } from '../apis/authentication.service';
 
 @Component({
@@ -9,9 +10,10 @@ import { AuthenticationService } from '../apis/authentication.service';
 })
 export class LoginPage implements OnInit {
   ionicForm: FormGroup;
-  login:any=[];
+  user:any=[];
+  login_response:any=[];
 
-  constructor(public formBuilder: FormBuilder, private service: AuthenticationService) { 
+  constructor(public formBuilder: FormBuilder, private service: AuthenticationService, private router: Router) { 
 
   }
 
@@ -24,10 +26,19 @@ export class LoginPage implements OnInit {
 
   submitForm() {
     console.log(this.ionicForm.value)
-    this.service.login(this.ionicForm.value).subscribe( response => {
-      this.login = response['data'];
+    this.service.login(this.ionicForm.value).subscribe( async (response) => {
+      this.user = response['data'];
+      this.login_response = response;
       console.log(response);
+
+      if(this.login_response['is_authenticated'] === 'true'){
+        this.router.navigate(['/tabs'])
+      }
+
     });
+
+
+    
   }
 
 }
