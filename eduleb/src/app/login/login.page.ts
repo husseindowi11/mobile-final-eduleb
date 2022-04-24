f9024788c2711681d8bb7c285fb02e1cfe5f763b
husseinadkerning <hseneldowi@gmail.com>
2022-04-24 21:33:40 +0300

    handle invalid credentials

--- eduleb/src/app/login/login.page.ts
+++ eduleb/src/app/login/login.page.ts
@@ -12,7 +12,9 @@ export class LoginPage implements OnInit {
   ionicForm: FormGroup;
   user:any=[];
   login_response:any=[];
-  isSubmitted = false;
+  is_submitted = false;
+  invalid_credentials = false;
+
   
 
   constructor(public formBuilder: FormBuilder, private service: AuthenticationService, private router: Router) { 
@@ -30,7 +32,7 @@ export class LoginPage implements OnInit {
   }
 
   submitForm() {
-    this.isSubmitted = true;
+    this.is_submitted = true;
     if (!this.ionicForm.valid) {
       console.log('Please provide all the required values!')
       return false;
@@ -41,7 +43,11 @@ export class LoginPage implements OnInit {
       this.login_response = response;
       console.log(response);
 
-      if(this.login_response['is_authenticated'] === 'true'){
+      if(this.login_response['is_authenticated'] === false){
+        this.invalid_credentials = true;
+      }
+
+      if(this.login_response['is_authenticated'] === true){
         this.router.navigate(['/tabs'])
       }
 
