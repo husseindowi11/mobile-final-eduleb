0f02b104b3473db5d31eda8e9413ea027dd80680
husseinadkerning <hseneldowi@gmail.com>
2022-04-24 21:44:04 +0300

    link register to apis

--- eduleb/src/app/register/register.page.ts
+++ eduleb/src/app/register/register.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../apis/authentication.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  ionicForm: FormGroup;
+  is_submitted = false;
+  register_response:any=[];
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder, private service: AuthenticationService, private router: Router) { }
 
   ngOnInit() {
+    this.ionicForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      password: ['', [Validators.required, Validators.minLength(2)]],
+    })
   }
 
+  get errorControl() {
+    return this.ionicForm.controls;
+  }
+
+  submitForm(){
+    this.is_submitted = true;
+    if (!this.ionicForm.valid) {
+      console.log('Please provide all the required values!')
+      return false;
+    }else{
+      console.log(this.ionicForm.value)
+      this.service.register(this.ionicForm.value).subscribe(response => {
+        this.register_response = response;
+        console.log(response);
+
+        // if(this.register_response['status_code'] != 200){
+        //   this.invalid_credentials = true;
+        // }
+
+        if(this.register_response['is_authenticated'] === 200){
+          this.router.navigate(['/login'])
+        }
+
+      });
+    }
+  }
+
+  
+ 
 }
