5cb880e4927e19bcde680ea93bd6354a0620c25a
husseinadkerning <hseneldowi@gmail.com>
2022-04-25 20:28:25 +0300

    add playVideo login in course-detail

--- eduleb/src/app/course-detail/course-detail.page.ts
+++ eduleb/src/app/course-detail/course-detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Course, CourseService } from '../apis/course.service';
+import { VideoPlayer } from '@awesome-cordova-plugins/video-player/ngx';
 
 @Component({
   selector: 'app-course-detail',
@@ -13,7 +14,7 @@ export class CourseDetailPage implements OnInit {
   details=[];
   id:any;
 
-  constructor(private router: Router,private route: ActivatedRoute, private service:CourseService) { }
+  constructor(private router: Router,private route: ActivatedRoute, private service:CourseService, private videoPlayer: VideoPlayer) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.queryParamMap.get('course');
@@ -41,6 +42,14 @@ export class CourseDetailPage implements OnInit {
       this.details = response['data'];
       console.log(this.details);
     });
+  }
+
+  playeVideo(){
+    this.videoPlayer.play('https://youtu.be/2ICc10-lUfQ').then(() => {
+      console.log('video completed');
+      }).catch(err => {
+      console.log(err);
+      });
   }
 
 }
